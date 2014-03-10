d305de5d97c36b9c8295bcdd892d701824417d73
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2014-03-10 01:16:21 -0500

    expansion matrices.

--- autoencoder.py
+++ autoencoder.py
@@ -20,16 +20,17 @@ parser.add_argument('--path', metavar='path', required=False, default='.',
                     help='Condor process number')
 args = parser.parse_args()
 
-visibleSize   = 28*28 # number of input units 
-hiddenSize    = 14*14 # number of hidden units 
-alpha         = 9e-1  # learning rate
-Lambda        = 3e-3  # weight decay term
-beta          = 3     # weight of sparsity penalty term       
-spar          = 0.1   # sparsity parameter
-useDCT        = True  # enable dct compression
-v             = visibleSize # Effective visual size
-h             = hiddenSize  # Effective hidden size
-path          = args.path
+visibleSize = 28*28 # number of input units 
+hiddenSize  = 14*14 # number of hidden units 
+alpha       = 9e-1  # learning rate
+Lambda      = 3e-3  # weight decay term
+beta        = 3     # weight of sparsity penalty term       
+spar        = 0.1   # sparsity parameter
+useDCT      = True  # enable dct compression
+v           = visibleSize # Effective visual size
+h           = hiddenSize  # Effective hidden size
+path        = args.path
+nStripes    = 6
 
 if useDCT and args.process_num is not None:
     assert int(args.process_num) > 0 and int(args.process_num) <= min(visibleSize,hiddenSize)
@@ -70,11 +71,15 @@ x = T.matrix('x')   # Training data
 # Initialize weights & biases
 r = np.sqrt(6) / np.sqrt(visibleSize+hiddenSize+1)
 
-theta = theano.shared(value=np.zeros(2*v*h+v+h,dtype=theano.config.floatX),name='theta',borrow=True)
-cW1 = theta[:v*h].reshape((v,h))
-cW2 = theta[v*h:2*v*h].reshape((h,v))
-cb1 = theta[2*v*h:2*v*h+h]
-cb2 = theta[2*v*h+h:]
+# W1 nStripes
+stripeWidth = 28 # How wide each stripe is
+nDctRows = int(stripeWidth * (nStripes-.5)) # Number of rows of dct coeffs we are keeping
+nDctCoeffs = hiddenSize * nDctRows # Number of dct coefficients for each weight matrix
+theta = theano.shared(value=np.zeros(2*nDctCoeffs+v+h,dtype=theano.config.floatX),name='theta',borrow=True)
+cW1 = theta[:nDctCoeffs].reshape((nDctRows, hiddenSize))
+cW2 = theta[nDctCoeffs:2*nDctCoeffs].reshape((nDctRows, hiddenSize))
+cb1 = theta[2*nDctCoeffs:2*nDctCoeffs+h]
+cb2 = theta[2*nDctCoeffs+h:]
 
 # Load saved matlab weights
 # import scipy.io
@@ -86,16 +91,37 @@ cb2 = theta[2*v*h+h:]
 
 if useDCT:
     # Create the DCT objects: currShape, targetShape
-    dct_cW1 = dct.dct(cW1.shape.eval(), (visibleSize, hiddenSize))
-    dct_cW2 = dct.dct(cW2.shape.eval(), (hiddenSize, visibleSize))
-    dct_cb1 = dct.dct(cb1.shape.eval(), (hiddenSize,))
-    dct_cb2 = dct.dct(cb2.shape.eval(), (visibleSize,))
+    dct_cW1 = dct.dct((visibleSize, hiddenSize))
+    dct_cW2 = dct.dct((hiddenSize, visibleSize))
+    # dct_cb1 = dct.dct(cb1.shape.eval(), (hiddenSize,))
+    # dct_cb2 = dct.dct(cb2.shape.eval(), (visibleSize,))
+
+    # Generate expansion matrix
+    E = np.zeros([visibleSize, nDctRows])
+    # First stripe - half width
+    sw = stripeWidth
+    E[:sw/2,:sw/2] = np.identity(sw/2)
+    for i in range(1,nStripes):
+        E[i*sw*2-sw/2:i*sw*2+sw/2, sw/2+(i-1)*sw:sw/2+i*sw] = np.identity(sw)
+
+    dW1 = T.dot(E, cW1)
+    dW2 = T.dot(E, cW2).T
+
+    # Gradients dont seem to propagate through this set_subtensor
+    # dW1 = T.zeros([visibleSize, hiddenSize])
+    # T.set_subtensor(dW1[0:7,:], cW1[:7*hiddenSize]) # Stripe 1
+    # T.set_subtensor(dW1[42:70,:], cW1[7*hiddenSize:(7+28)*hiddenSize])
+    # dW2 = T.zeros([hiddenSize, visibleSize])
+    # T.set_subtensor(dW2[:,0:7], cW2[:7*hiddenSize])
+    # T.set_subtensor(dW2[:,42:70], cW2[7*hiddenSize:(7+28)*hiddenSize])
 
     # Expand the coefficients into larger matrices
-    W1 = dct_cW1.idct2(cW1)
-    W2 = dct_cW2.idct2(cW2)
-    b1 = dct_cb1.idct(cb1)
-    b2 = dct_cb2.idct(cb2)
+    W1 = dct_cW1.idct2(dW1)
+    W2 = dct_cW2.idct2(dW2)
+    # b1 = dct_cb1.idct(cb1)
+    # b2 = dct_cb2.idct(cb2)
+    b1 = cb1
+    b2 = cb2
 
 else:
     W1 = cW1
@@ -166,18 +192,21 @@ callbackFn.epoch = 0
 # predict = theano.function(inputs=[x], outputs=[cost], allow_input_downcast=True)
 # print predict(patches)
 
-x0 = np.concatenate(((rng.randn(2*v*h)*2*r-r).flatten(),np.zeros(v+h))).astype('float32')
-if useDCT:
-    # Find coefficients that expand to the correct initial weight matrices
-    dctShrink_cW1 = dct.dct((visibleSize, hiddenSize))
-    iW1 = dctShrink_cW1.dct2(rng.randn(visibleSize, hiddenSize) * 2 * r - r)[:v,:h]
-    dctShrink_cW2 = dct.dct((hiddenSize, visibleSize))
-    iW2 = dctShrink_cW2.dct2(rng.randn(hiddenSize, visibleSize) * 2 * r - r)[:h,:v]
-    dctShrink_cb1 = dct.dct((hiddenSize,))
-    ib1 = dctShrink_cb1.dct(np.zeros(hiddenSize))[:h]
-    dctShrink_cb2 = dct.dct((visibleSize,))
-    ib2 = dctShrink_cb2.dct(np.zeros(visibleSize))[:v]
-    x0 = np.concatenate([iW1.flatten(),iW2.flatten(),ib1,ib2]).astype('float32')
+
+x0 = np.concatenate(((rng.randn(2*nDctCoeffs)*2*r-r).flatten(),np.zeros(v+h))).astype('float32')
+# if useDCT:
+#     # Find coefficients that expand to the correct initial weight matrices
+#     dctShrink_cW1 = dct.dct((visibleSize, hiddenSize))
+#     iW1 = dctShrink_cW1.dct2(rng.randn(visibleSize, hiddenSize) * 2 * r - r)[:v,:h]
+#     dctShrink_cW2 = dct.dct((hiddenSize, visibleSize))
+#     iW2 = dctShrink_cW2.dct2(rng.randn(hiddenSize, visibleSize) * 2 * r - r)[:h,:v]
+#     dctShrink_cb1 = dct.dct((hiddenSize,))
+#     ib1 = dctShrink_cb1.dct(np.zeros(hiddenSize))[:h]
+#     dctShrink_cb2 = dct.dct((visibleSize,))
+#     ib2 = dctShrink_cb2.dct(np.zeros(visibleSize))[:v]
+#     x0 = np.concatenate([iW1.flatten(),iW2.flatten(),ib1,ib2]).astype('float32')
+
+
 
 training_epochs = 200
 start = time.time()
@@ -190,8 +219,9 @@ opttheta = scipy.optimize.fmin_cg(
 
 fname = path + '/results/' + (str(args.process_num) if args.process_num else 'final') + '.png'
 theta.set_value(opttheta, borrow=True)
+Z = dct_cW1.idct2(T.dot(E,cW1)).eval().T
 image = PIL.Image.fromarray(tile_raster_images(
-        X=dct_cW1.idct2(cW1).eval().T if useDCT else cW1.eval().T,
+        X=Z,#dct_cW1.idct2(dW1).eval().T if useDCT else cW1.eval().T,
         img_shape=(28, 28), tile_shape=(14, 14),
         tile_spacing=(1, 1)))
 image.save(fname)
