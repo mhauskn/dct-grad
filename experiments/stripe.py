c2586b7ce4efd23c06ee60a83786b03a62859c7d
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2014-03-12 16:48:31 -0500

    More work on condor submit helper.

---
+++ experiments/stripe.py
@@ -0,0 +1,12 @@
+import condor
+
+nTrials = 10
+nStripes = 10
+
+cj = condor.job('/lusr/bin/python')
+
+for trial in range(nTrials):
+    for stripe in range(nStripes):
+        cj.setArgs('/u/mhauskn/projects/dct-grad/autoencoder.py --nStripes %d --path /u/mhauskn/projects/dct-grad/'%stripe)
+        cj.setOutputPrefix('/u/mhauskn/projects/dct-grad/results/t%d_%d'%(trial,stripe))
+        cj.submit()
