b390ac28dd651e83df2ac065a2d0ad7787602e8d
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2014-04-02 11:54:15 -0500

    .

--- experiments/stripeVSreshape.py
+++ experiments/stripeVSreshape.py
@@ -1,21 +1,23 @@
 # This experiment is meant to test the power of dct encoding using a rectangle of coeffs v. stripes v. reshaping
 import condor, socket, subprocess
 
+local = socket.gethostname() == 'drogba'
 nStripes = 10
-execStr = 'python' if socket.gethostname() == 'drogba' else '/lusr/bin/python'
+execStr = 'python' if local else '/lusr/bin/python'
+expDir = '/home/matthew/projects/dct-grad/' if local else '/u/mhauskn/projects/dct-grad/'
 cj = condor.job(execStr)
 for stripe in range(1,nStripes):
-    cj.setArgs('/home/matthew/projects/dct-grad/stripeAE.py --nStripes %d --path /home/matthew/projects/dct-grad/ --outputPrefix stripe%d'%(stripe,stripe))
-    cj.setOutputPrefix('/home/matthew/projects/dct-grad/results/stripe%d'%stripe)
-    cj.runLocal() if socket.gethostname() == 'drogba' else cj.submit()
+    cj.setArgs('%s/stripeAE.py --nStripes %d --path %s --outputPrefix stripe%d'%(expDir,stripe,expDir, stripe))
+    cj.setOutputPrefix('%s/results/stripe%d'%(expDir,stripe))
+    cj.runLocal() if local else cj.submit()
 
     nStripeParams = int(28 * (stripe - .5)) * 196 * 2 + 196 + 784
     perc = nStripeParams/float(308308) # divide by total params
 
-    cj.setArgs('/home/matthew/projects/dct-grad/reshapingAE.py --compression %f --path /home/matthew/projects/dct-grad/ --outputPrefix reshape%d'%(perc,stripe))
-    cj.setOutputPrefix('/home/matthew/projects/dct-grad/results/reshape%d'%stripe)
-    cj.runLocal() if socket.gethostname() == 'drogba' else cj.submit()
+    cj.setArgs('%s/reshapingAE.py --compression %f --path %s --outputPrefix reshape%d'%(expDir,perc,expDir,stripe))
+    cj.setOutputPrefix('%s/results/reshape%d'%(expDir,stripe))
+    cj.runLocal() if local else cj.submit()
     
-    cj.setArgs('/home/matthew/projects/dct-grad/rectangleAE.py --compression %f --path /home/matthew/projects/dct-grad/ --outputPrefix rectangle%d'%(perc,stripe))
-    cj.setOutputPrefix('/home/matthew/projects/dct-grad/results/rectangle%d'%stripe)
-    cj.runLocal() if socket.gethostname() == 'drogba' else cj.submit()
+    cj.setArgs('%s/dct-grad/rectangleAE.py --compression %f --path %s --outputPrefix rectangle%d'%(expDir,perc,expDir,stripe))
+    cj.setOutputPrefix('%s/results/rectangle%d'%(expDir,stripe))
+    cj.runLocal() if local else cj.submit()
