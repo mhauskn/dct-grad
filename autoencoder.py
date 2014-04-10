27bf648a4197dee90aa7ba1a42b85c5f2c30ded8
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2014-04-10 13:15:00 -0500

    Refactored autoencoders.

---
+++ autoencoder.py
@@ -0,0 +1,209 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import dct
+
+rng = np.random
+
+def sparsityCost(a1, spar):
+    avgAct = a1.mean(axis=0) # Col-Mean: AvgAct of each hidden unit across all m-examples
+    return T.sum(spar * T.log(spar/avgAct) + (1-spar) * T.log((1-spar)/(1-avgAct)))
+
+def reconstructionCost(x, a):
+    return T.sum((a - x) ** 2) / (2. * x.shape[0])
+
+class Autoencoder():
+    def __init__(self, visibleSize, hiddenSize):
+        self.nWeightParams = 2*visibleSize*hiddenSize
+        self.nBiasParams   = visibleSize + hiddenSize
+        self.nParams       = self.nWeightParams + self.nBiasParams
+        print "Direct Autoencoder\n%d total parameters"%self.nParams
+
+        self.theta = theano.shared(value=np.zeros(self.nParams,dtype=theano.config.floatX),
+                                   name='theta',borrow=True)
+        self.W1 = self.theta[:visibleSize*hiddenSize].reshape((visibleSize, hiddenSize))
+        self.W2 = self.theta[visibleSize*hiddenSize:2*visibleSize*hiddenSize].reshape((hiddenSize,visibleSize))
+        self.b1 = self.theta[2*visibleSize*hiddenSize:2*visibleSize*hiddenSize+hiddenSize]
+        self.b2 = self.theta[2*visibleSize*hiddenSize+hiddenSize:]
+
+        r = np.sqrt(6) / np.sqrt(visibleSize+hiddenSize+1)
+        self.x0 = np.concatenate(((rng.randn(self.nWeightParams)*2*r-r).flatten(),np.zeros(self.nBiasParams))).astype('float32')
+
+    def forward(self, x):
+        a1 = T.nnet.sigmoid(T.dot(x, self.W1) + self.b1)
+        a2 = T.nnet.sigmoid(T.dot(a1, self.W2) + self.b2)
+        return a1, a2
+
+    def weightCost(self):
+        return T.sum(self.W1**2) + T.sum(self.W2**2)
+        
+    def saveImage(self, outputPrefix, opttheta):
+        fname = path + '/results/' + outputPrefix + '.png'
+        self.theta.set_value(opttheta, borrow=True)
+        image = PIL.Image.fromarray(tile_raster_images(
+                X=self.W1.eval().T,
+                img_shape=(28, 28), tile_shape=(14, 14),
+                tile_spacing=(1, 1)))
+        image.save(fname)
+        
+
+class RectangleAE(Autoencoder):
+    def __init__(self, visibleSize, hiddenSize, compression=.5):
+        nWeightParams = 2*visibleSize*hiddenSize
+        nBiasParams   = visibleSize + hiddenSize
+        nParams = nWeightParams + nBiasParams
+
+        self.dctW1Shape = (int(np.round(np.sqrt(compression*visibleSize*visibleSize))),
+                           int(np.round(np.sqrt(compression*hiddenSize*hiddenSize))))
+        self.dctW2Shape = (self.dctW1Shape[1],self.dctW1Shape[0])
+        self.dctWeightSize = self.dctW1Shape[0]*self.dctW1Shape[1]
+        self.nDCTParams = 2 * self.dctWeightSize + nBiasParams
+
+        print "RectangleAE learning in DCT space\n%d total parameters (%f%%)\nDCT input shape"\
+            %(self.nDCTParams,100.*self.nDCTParams/nParams), self.dctW1Shape
+
+        self.theta = theano.shared(value=np.zeros(self.nDCTParams,dtype=theano.config.floatX),name='theta',borrow=True)
+
+        self.cW1 = self.theta[:self.dctWeightSize].reshape(self.dctW1Shape)
+        self.cW2 = self.theta[self.dctWeightSize:2*self.dctWeightSize].reshape(self.dctW2Shape)
+        self.b1 = self.theta[2*self.dctWeightSize:2*self.dctWeightSize+hiddenSize]
+        self.b2 = self.theta[2*self.dctWeightSize+hiddenSize:]
+
+        self.d1 = dct.dct(self.dctW1Shape,(visibleSize,hiddenSize))
+        self.d2 = dct.dct(self.dctW2Shape,(hiddenSize,visibleSize))
+
+        self.W1 = self.d1.idct2(self.cW1)
+        self.W2 = self.d2.idct2(self.cW2)
+    
+        r = np.sqrt(6) / np.sqrt(visibleSize+hiddenSize+1)
+        x0 = np.concatenate(((rng.randn(nWeightParams)*2*r-r).flatten(),np.zeros(nBiasParams))).astype('float32')
+        x0w1 = x0[:visibleSize*hiddenSize].reshape((visibleSize, hiddenSize))
+        dctShrink_cW1 = dct.dct((visibleSize, hiddenSize))
+        iW1 = dctShrink_cW1.dct2(x0w1)[:self.dctW1Shape[0],:self.dctW1Shape[1]]
+        x0w2 = x0[visibleSize*hiddenSize:2*visibleSize*hiddenSize].reshape((hiddenSize,visibleSize))
+        dctShrink_cW2 = dct.dct((hiddenSize, visibleSize))
+        iW2 = dctShrink_cW2.dct2(x0w2)[:self.dctW2Shape[0],:self.dctW2Shape[1]]
+        self.x0 = np.concatenate([iW1.flatten(),iW2.flatten(),np.zeros(nBiasParams)]).astype('float32')
+
+    def weightCost(self):
+        return T.sum(self.cW1**2) + T.sum(self.cW2**2)
+
+        # pdf = np.vectorize(scipy.stats.norm().pdf)
+        # w1tmp = np.outer(pdf(np.linspace(0,2,dctW1Shape[0])),pdf(np.linspace(0,2,dctW1Shape[1])))
+        # cW1Penalty = 1.-(w1tmp/np.max(w1tmp))
+        # w2tmp = np.outer(pdf(np.linspace(0,2,dctW2Shape[0])),pdf(np.linspace(0,2,dctW2Shape[1])))
+        # cW2Penalty = 1.-(w2tmp/np.max(w2tmp))
+        # penW1 = theano.shared(value=cW1Penalty.astype('float32'),borrow=True)    
+        # penW2 = theano.shared(value=cW2Penalty.astype('float32'),borrow=True)
+        # return T.sum(penW1 * T.abs_(cW1)) + T.sum(penW2 * T.abs_(cW2))
+        
+class StripeAE(Autoencoder):
+    def __init__(self, visibleSize, hiddenSize, nStripes=9):
+        nWeightParams = 2*visibleSize*hiddenSize
+        nBiasParams = visibleSize + hiddenSize
+        nParams = nWeightParams + nBiasParams
+        stripeWidth = int(np.round(np.sqrt(visibleSize)))
+        nDctRows = int(stripeWidth * (nStripes-.5)) # Number of rows of dct coeffs we are keeping
+        nDctCoeffs = hiddenSize * nDctRows # Number of dct coefficients for each weight matrix
+        nDCTWeightParams = 2*nDctCoeffs 
+        nDCTParams = nDCTWeightParams + nBiasParams
+
+        print "StripeAE learning in DCT Space\n%d stripes of width %d\n%d total parameters (%f%%)"\
+            %(nStripes,stripeWidth,nDCTParams,100.*nDCTParams/nParams)
+
+        self.theta = theano.shared(value=np.zeros(nDCTParams,dtype=theano.config.floatX),name='theta',borrow=True)
+
+        self.cW1 = self.theta[:nDctCoeffs].reshape((nDctRows, hiddenSize))
+        self.cW2 = self.theta[nDctCoeffs:2*nDctCoeffs].reshape((nDctRows, hiddenSize))
+        self.b1 = self.theta[2*nDctCoeffs:2*nDctCoeffs+hiddenSize]
+        self.b2 = self.theta[2*nDctCoeffs+hiddenSize:]
+
+        self.dctW1 = dct.dct((visibleSize, hiddenSize)) # Create the DCT transform
+        self.dctW2 = dct.dct((hiddenSize, visibleSize))
+
+        E = np.zeros([visibleSize, nDctRows]) # Generate expansion matrix E
+        sw = stripeWidth
+        E[:sw/2,:sw/2] = np.identity(sw/2) # First stripe - half width
+        for i in range(1,nStripes):
+            E[i*sw*2-sw/2:i*sw*2+sw/2, sw/2+(i-1)*sw:sw/2+i*sw] = np.identity(sw)
+
+        self.sE = theano.shared(value=E.astype('float32'),name='E',borrow=True)
+
+        self.dW1 = T.dot(self.sE, self.cW1)   # Expand coefficients
+        self.dW2 = T.dot(self.sE, self.cW2).T
+
+        self.W1 = self.dctW1.idct2(self.dW1) # Inverse DCT transform
+        self.W2 = self.dctW2.idct2(self.dW2)
+
+        r = np.sqrt(6) / np.sqrt(visibleSize+hiddenSize+1)
+        dctShrink_cW1 = dct.dct((visibleSize, hiddenSize))
+        iW1 = np.dot(E.T,dctShrink_cW1.dct2(rng.randn(visibleSize, hiddenSize)*2*r-r))
+        dctShrink_cW2 = dct.dct((hiddenSize, visibleSize))
+        iW2 = np.dot(E.T,dctShrink_cW2.dct2(rng.randn(hiddenSize, visibleSize)*2*r-r).T).T
+        self.x0 = np.concatenate([iW1.flatten(),iW2.flatten(),np.zeros(nBiasParams)]).astype('float32')
+
+    def weightCost(self):
+        return T.sum(self.cW1**2) + T.sum(self.cW2**2)
+        # pdf = np.vectorize(scipy.stats.norm().pdf)
+        # wtmp = np.outer(pdf(np.linspace(0,2,nDctRows)),pdf(np.linspace(0,2,hiddenSize)))
+        # cWPenalty = 1.-(wtmp/np.max(wtmp))
+        # penW = theano.shared(value=cWPenalty.astype('float32'),borrow=True)    
+        # return T.sum(penW * T.abs_(cW1)) + T.sum(penW * T.abs_(cW2))
+
+class ReshapeAE(Autoencoder):
+    def __init__(self, visibleSize, hiddenSize, inputShape, compression=.5):    
+        nWeightParams = 2*visibleSize*hiddenSize
+        nBiasParams = visibleSize + hiddenSize
+        nParams = nWeightParams + nBiasParams
+        dctShape = (int(np.round(np.sqrt(inputShape[0]*compression*visibleSize/float(inputShape[1])))),
+                    int(np.round(np.sqrt(inputShape[1]*compression*visibleSize/float(inputShape[0])))))
+        dctVisibleSize = dctShape[0]*dctShape[1]
+        nDCTParams = 2*dctVisibleSize*hiddenSize + nBiasParams
+
+        print "ReshapeAE learning in DCT space\n%d total parameters (%f%%)\nDCT input shape"\
+            %(nDCTParams,100.*nDCTParams/nParams), dctShape
+
+
+        self.theta = theano.shared(value=np.zeros(nDCTParams,dtype=theano.config.floatX),name='theta',borrow=True)
+
+        self.cW1 = self.theta[:dctVisibleSize*hiddenSize].reshape((dctVisibleSize, hiddenSize))
+        self.cW2 = self.theta[dctVisibleSize*hiddenSize:2*dctVisibleSize*hiddenSize].reshape((hiddenSize,dctVisibleSize))
+        self.b1 = self.theta[2*dctVisibleSize*hiddenSize:2*dctVisibleSize*hiddenSize+hiddenSize]
+        self.b2 = self.theta[2*dctVisibleSize*hiddenSize+hiddenSize:]
+
+        self.d = dct.dct(dctShape,inputShape) # Create the DCT transform
+
+        x = T.matrix('x')
+        self.W1, _ = theano.scan(fn=lambda x: self.d.idct2(x.reshape(dctShape)).flatten(),
+                                 outputs_info=None,
+                                 sequences=[self.cW1.T])
+        self.W1 = self.W1.T
+
+        self.W2, _ = theano.scan(fn=lambda x: self.d.idct2(x.reshape(dctShape)).flatten(),
+                                 outputs_info=None,
+                                 sequences=[self.cW2])
+
+        r = np.sqrt(6) / np.sqrt(visibleSize+hiddenSize+1)
+        x0 = np.concatenate(((rng.randn(nWeightParams)*2*r-r).flatten(),np.zeros(nBiasParams)))
+        mydct = dct.dct(inputShape)
+        x0w1 = x0[:visibleSize*hiddenSize].reshape((visibleSize, hiddenSize))
+        x0w2 = x0[visibleSize*hiddenSize:2*visibleSize*hiddenSize].reshape((hiddenSize,visibleSize))
+        tmpW1 = np.vstack([mydct.dct2(x0w1[:,i].reshape(inputShape))[:dctShape[0],:dctShape[1]].flatten() for i in xrange(hiddenSize)]).T
+        tmpW2 = np.vstack([mydct.dct2(x0w2[i].reshape(inputShape))[:dctShape[0],:dctShape[1]].flatten() for i in xrange(hiddenSize)])
+        self.x0 = np.concatenate([tmpW1.flatten(),tmpW2.flatten(),np.zeros(nBiasParams)]).astype('float32')
+
+        def weightCost(self):
+            # L2-Regularization
+            return T.sum(self.cW1**2) + T.sum(self.cW2**2)
+            # L1-Regularization
+            # return T.sum(T.abs_(cW1)) + T.sum(T.abs_(cW2))
+
+            # Frequency weighted L2-Regularization
+            # pdf = np.vectorize(scipy.stats.norm().pdf)
+            # # Create the penalty gaussian for the dct-image
+            # wtmp = np.outer(pdf(np.linspace(0,2,dctShape[0])),pdf(np.linspace(0,2,dctShape[1])))
+            # imgPenalty = 1.-(wtmp/np.max(wtmp))
+            # # Flatten and tile this into a matrix of size [dctVisibleSize, hiddenSize]
+            # cWPenalty = np.tile(imgPenalty.flatten(),(hiddenSize,1)).T
+            # penW = theano.shared(value=cWPenalty.astype('float32'),borrow=True)    
+            # return T.sum(penW * (cW1**2)) + T.sum(penW.T * (cW2**2))
---
+++ train.py
@@ -0,0 +1,127 @@
+import sys
+import time
+import numpy as np
+import theano
+import theano.tensor as T
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import mnist
+from utils import tile_raster_images
+import PIL.Image
+import scipy.optimize
+import argparse
+from autoencoder import *
+
+parser = argparse.ArgumentParser(description='Testing dct transforms')
+parser.add_argument('--autoencoder', required=False, type=str, default='autoencoder')
+parser.add_argument('--compression', required=False, type=float, default=.5)
+parser.add_argument('--nEpochs', required=False, type=int, default=200)
+parser.add_argument('--outputPrefix', required=False, type=str, default='out')
+parser.add_argument('--path', required=False, default='.')
+parser.add_argument('--noKLDiv', action='store_true', default=False)
+parser.add_argument('--noWeightCost', action='store_true', default=False)
+parser.add_argument('--noDCTWeightCost', action='store_true', default=False)
+parser.add_argument('--dataDCT', action='store_true', default=False)
+parser.add_argument('--Lambda', required=False, type=float, default=3e-3)
+parser.add_argument('--dctLambda', required=False, type=float, default=3e-3)
+args = parser.parse_args()
+
+#=================== Parameters ===========================#
+inputShape    = (28, 28)             # Dimensionality of input 
+visibleSize   = 28*28                # Number of input units 
+hiddenSize    = 14*14                # Number of hidden units 
+Lambda        = args.Lambda          # Weight decay term
+dctLambda     = args.dctLambda       # DCT-Weight decay term
+beta          = 3                    # Weight of sparsity penalty term       
+spar          = 0.1                  # Sparsity parameter
+compression   = args.compression     # Percentage compression
+path          = args.path            # Directory to load/save files
+outputPrefix  = args.outputPrefix    # Prefix for output file names
+trainEpochs   = args.nEpochs         # How many epochs to train
+dataDCT       = args.dataDCT         # Performs DCT transform on the dataset
+aeType        = args.autoencoder     # What type of autoencoder
+
+#================== Load the dataset ==========================#
+if dataDCT: print 'Applying 2d-DCT transform to the dataset.'
+images = mnist.read(range(10),'training',path, dataDCT)[0]
+# plt.imshow(images[0,:].reshape(28,28), cmap=cm.Greys_r)
+train_set_x = theano.shared(np.asarray(images[:10000], dtype=theano.config.floatX))
+images = mnist.read(range(10),'testing',path, dataDCT)[0]
+test_set_x = theano.shared(np.asarray(images, dtype=theano.config.floatX))
+
+nTrain        = train_set_x.shape[0].eval() # Number training samples
+nTest         = test_set_x.shape[0].eval()  # Number of test samples
+batch_size    = nTrain                      # Size of minibatches
+nTrainBatches = nTrain / batch_size         # Number of minibatches
+nTestBatches  = nTest / batch_size          
+
+if aeType == 'autoencoder':
+    ae = Autoencoder(visibleSize, hiddenSize)
+elif aeType == 'rectangle':
+    ae = RectangleAE(visibleSize, hiddenSize)
+elif aeType == 'stripe':
+    ae = StripeAE(visibleSize, hiddenSize)
+elif aeType == 'reshape':
+    ae = ReshapeAE(visibleSize, hiddenSize, inputShape)
+else:
+    print 'Got unexpected autoencoder type', aeType
+    exit()
+
+index = T.lscalar()      # Index into the batch of training examples
+x = T.matrix('x')        # Training data 
+a1, a2 = ae.forward(x)   # Forward Propagate
+cost = reconstructionCost(x,a2) + beta * sparsityCost(a1,spar) + Lambda * ae.weightCost()
+
+#================== Theano Functions ==========================#
+batch_cost = theano.function( # Compute the cost of a minibatch
+    inputs=[index],
+    outputs=cost,
+    givens={x:train_set_x[index * batch_size: (index + 1) * batch_size]},
+    name="batch_cost")
+
+test_cost = theano.function( # Compute the cost of a minibatch
+    inputs=[index],
+    outputs=cost,
+    givens={x:test_set_x[index * batch_size: (index + 1) * batch_size]},
+    name="batch_cost")
+
+batch_grad = theano.function( # Compute the gradient of a minibatch
+    inputs=[index],
+    outputs=T.grad(cost, ae.theta),
+    givens={x:train_set_x[index * batch_size: (index + 1) * batch_size]},
+    name="batch_grad")
+
+def trainFn(theta_value):
+    ae.theta.set_value(theta_value, borrow=True)
+    train_losses = [batch_cost(i * batch_size) for i in xrange(nTrainBatches)]
+    meanLoss = np.mean(train_losses)
+    return meanLoss
+
+def gradFn(theta_value):
+    ae.theta.set_value(theta_value, borrow=True)
+    grad = batch_grad(0)
+    for i in xrange(1, nTrainBatches):
+        grad += batch_grad(i * batch_size)
+    return grad / nTrainBatches
+
+def callbackFn(theta_value):
+    ae.theta.set_value(theta_value, borrow=True)
+    train_losses = [batch_cost(i) for i in xrange(nTrainBatches)]
+    test_losses = [test_cost(i) for i in xrange(nTestBatches)]
+    print 'Epoch %d Train: %f Test: %f'%(callbackFn.epoch,np.mean(train_losses),np.mean(test_losses))
+    sys.stdout.flush()
+    callbackFn.epoch += 1
+callbackFn.epoch = 0
+
+#================== Optimize ==========================#
+start = time.time()
+opttheta = scipy.optimize.fmin_cg(
+    f=trainFn,
+    x0=ae.x0,
+    fprime=gradFn,
+    callback=callbackFn,
+    maxiter=trainEpochs)
+end = time.time()
+print 'Elapsed Time(s): ', end - start
+
+ae.saveImage(outputPrefix, opttheta)
